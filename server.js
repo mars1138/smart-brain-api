1c81c1a8d1f6bcdd3c191d0e4b8497ce36c6e1ee
LearnToCode1138 <mdelgado1138@hotmail.com>
2021-08-22 21:09:30 -0700

    update register with transaction

--- server.js
+++ server.js
@@ -69,17 +69,42 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then(user => {
-      res.json(user[0]); // make sure we are not potentially returning an array
-    })
-    .catch(err => res.status(400).json('Unable to register!'));
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('Unable to register!'));
+
+  // return db('users')
+  //   .returning('*')
+  //   .insert({
+  //     email: email,
+  //     name: name,
+  //     joined: new Date(),
+  //   })
+  //   .then(user => {
+  //     res.json(user[0]); // make sure we are not potentially returning an array
+  //   })
+
   // bcrypt.hash(password, null, null, function (err, hash) {
   //   console.log(hash);
   // });
