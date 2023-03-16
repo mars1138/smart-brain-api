10e9210cfa6d8f11b878ddd88eeb5b83323427c5
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-03-15 20:19:29 -0700

    update knex/db url settings

--- server.js
+++ server.js
@@ -9,26 +9,26 @@ const profile = require('./controllers/profile');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DB_URL,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-
-// TESTING;
 const db = knex({
-  client: `${process.env.DB_CLIENT}`,
+  client: 'pg',
   connection: {
-    host: `${process.env.DB_HOST}`,
-    user: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PASSWORD}`,
-    database: `${process.env.DB_NAME}`,
+    connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
+
+// TESTING;
+// const db = knex({
+//   client: `${process.env.DB_CLIENT}`,
+//   connection: {
+//     host: `${process.env.DB_HOST}`,
+//     user: `${process.env.DB_USER}`,
+//     password: `${process.env.DB_PASSWORD}`,
+//     database: `${process.env.DB_NAME}`,
+//   },
+// });
 
 // db.select('*')
 //   .from('users')
